aeeefb7f94b7face85feaad5b9f3c3471534f2e3
pihasi <jagaimo01414@gmail.com>
2022-02-20 22:55:07 +0900

    a

--- RpgTest/js/engine/game.js
+++ RpgTest/js/engine/game.js
@@ -1,111 +1,93 @@
 'use strict'
 
-/**
- * ゲームづくりの基本となるクラス
- */
-class Game {
+const widthCanvas = 320;
+const heightCanvas = 320;
 
-  /**
-   * 引数
-   * width : ゲームの横幅
-   * height : ゲームの縦幅
-   */
-  constructor(width, height) {
-    //canvas要素を作成
-    this.canvas = document.createElement('canvas');
-    //作成したcanvas要素をbodyタグに追加
-    document.body.appendChild(this.canvas);
-    //canvasの横幅（ゲームの横幅）を設定。もし横幅が指定されていなければ320を代入
-    this.canvas.width = width || 320;
-    //canvasの縦幅（ゲームの縦幅）を設定。もし縦幅が指定されていなければ320を代入
-    this.canvas.height = height || 320;
+var sprits = [];
+var backGround = null;
+
+var isNeedUpdate = false;
+
+var canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    canvas.width = widthCanvas;
+    canvas.height = heightCanvas;
     
-    this.backGround = null;
+    canvas.addEventListener("click", searchForClickedSprite);
 
-    //ゲームに登場する全てのもの（オブジェクト）を入れるための配列
-    this.objs = [];
-  } //constructor() 終了
-  
-  setBackGround(img){
+function setBackGround(img){
     if( img != null) {
-      this.backGround = new Image();
-      this.backGround.src = img;
+      backGround = new Image();
+      backGround.src = img;
     } else {
-      this.backGround = null;
+      backGround = null;
     }
   }
+  
+function addSprite(sprite) {
+    //sprits配列の末尾に、objの値を追加
+    sprits.push(sprite);
+  } //add() 終了
+  
+function killAllSprits(){
+  sprits =[];
+}
 
-  /**
-   * startメソッドを呼び出すことで、メインループが開始される
-   */
-  start() {
-    //メインループを呼び出す
-    this._mainLoop();
-    
-    this.canvas.addEventListener("click", function(e){
+
+  
+function searchForClickedSprite(e){
     	  let clickX = e.pageX;
     	  let clickY = e.pageY;
     	  
-        for (let i = 0; i < this.objs.length; i++) {
-          let spriteX = this.objs[i].x;
+        for (let i = 0; i < sprits.length; i++) {
+          let spriteX = sprits[i].x;
           if(clickX < spriteX) continue;
           
-          let spriteW = this.objs[i].width;
+          let spriteW = sprits[i].width;
           if(clickX > spriteX + spriteW) continue;
           
-          let spriteY = this.objs[i].y;
+          let spriteY = sprits[i].y;
           if(clickY < spriteY) continue;
           
-          let spriteH = this.objs[i].height;
+          let spriteH = sprits[i].height;
           if(clickY > spriteY + spriteH) continue;
           
-          this.objs[i].clicked();
+          sprits[i].clicked();
           break;
         }
+}
     
-    });
-  }
+
+function startCanvasUpdate() {
+  isNeedUpdate = true;
+  loopCanvasUpdate();
+}
     
   
 
 
-  /**
-   * メインループ
-   */
-  _mainLoop() {
-    //画家さん（コンテキスト）を呼ぶ
-    const ctx = this.canvas.getContext('2d');
-    
-    if(this.backGround == null) {
+function loopCanvasUpdate() {
+  if(isNeedUpdate){
+    const ctx = canvas.getContext('2d');
+    if(backGround == null) {
       //塗りつぶしの色に、黒を指定する
       ctx.fillStyle = '#000000';
       //左上から、画面のサイズまでを、塗りつぶす
-      ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
     } else {
-      ctx.drawImage(this.backGround,0,0,
-        this.canvas.width, this.canvas.height);
+      ctx.drawImage(backGround,0,0,
+        canvas.width, canvas.height);
     }
     //ゲームに登場する全てのもの（オブジェクト）の数だけ繰り返す
-    for (let i = 0; i < this.objs.length; i++) {
+    for (let i = 0; i < sprits.length; i++) {
       //スプライトやテキストなど、すべてのオブジェクトのupdateメソッドを呼び出す
-      this.objs[i].update(this.canvas);
+      sprits[i].update(canvas);
     }
 
     //自分自身（_mainLoop）を呼び出して、ループさせる
-    requestAnimationFrame(this._mainLoop.bind(this));
-  } //_mainLoop() 終了
-
-  /**
-   * オブジェクトをゲームに追加できるようになる、addメソッドを作成
-   *
-   * 引数
-   * obj : 追加したいオブジェクト
-   */
-  add(obj) {
-    //this.objs配列の末尾に、objの値を追加
-    this.objs.push(obj);
-  } //add() 終了
-
+    requestAnimationFrame(loopCanvasUpdate);
+  }
 }
+
 
 
--- RpgTest/js/main.js
+++ RpgTest/js/main.js
@@ -2,32 +2,28 @@
 
 //ブラウザがページを完全に読みこむまで待つ
 addEventListener('load', () => {
-
-  //変数gameに、あなたはゲームですよ、と教える
-  const game = new Game();
-
   const protagonist = new Sprite('pic/hero.png');
   protagonist.setx(64*2);
   protagonist.sety(64*4);
-  game.add(protagonist);
+  addSprite(protagonist);
   
   const house = new Sprite('pic/house.png');
   house.setx(64*2);
   house.sety(0);
-  game.add(house);
+  addSprite(house);
 
   const baby = new Sprite('pic/baby-smile-male.png');
   baby.setx(64*2);
   baby.sety(64*2);
-  game.add(baby);
+  addSprite(baby);
 
   
   
   
   
-  game.setBackGround("pic/roadOnGrass.png");
+  setBackGround("pic/roadOnGrass.png");
 
   //gameに、ゲームをスタートして、とお願いする
-  game.start();
+  startCanvasUpdate();
 
 });
